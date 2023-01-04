aa801ceaa598d582b7aba75b85515c512dfe3c26
Alibek <musicchannel0p@gmail.com>
2023-01-05 01:36:44 +0600

    started making header component, half made a filter, at the moment its called choose, but im so tired to change, so ill just leave it until tomorrow

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import banner from "./assets/header.jpg";
+
 
 import {
   Blog,
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,8 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import "./header.css";
+import { SlArrowDown } from "react-icons/sl";
 
 const Header = () => {
-  return <div>Header</div>;
+  const [selectClicked, setSelectClicked] = useState("");
+  const [caretRotate, setCaretRotate] = useState("");
+  const [menu, setMenu] = useState("");
+
+  const [active, setActive] = useState("");
+
+  const options = [
+    "e-tron S",
+    "e-tron Sportback",
+    "e-tron S Sportback",
+  ];
+
+  return (
+    <div className="header">
+      <div className="header__title">
+        <h1>Die besten Vorsätze fahren elektrisch</h1>
+        <button type="button">Mehr erfahren</button>
+      </div>
+      <div className="header__black-line"></div>
+      <div className="header__choose">
+        <div className="dropdown">
+          <div
+            onClick={() => {
+              setSelectClicked(!selectClicked);
+              setCaretRotate(!caretRotate);
+              setMenu(!menu);
+            }}
+            className={selectClicked ? "select-clicked" : "select"}
+          >
+            <div className="selected">
+              <span>{active ? active.value : "choose your car"}</span>
+            </div>
+            <div className={caretRotate ? "caret-rotate" : "caret"}>
+              <SlArrowDown />
+            </div>
+          </div>
+          {menu && (
+            <ul className={menu ? "menu-open" : "menu"}>
+              {options.map((value, index) => (
+                <li
+                  onClick={() => {
+                    setSelectClicked(!selectClicked);
+                    setCaretRotate(!caretRotate);
+                    setMenu(!menu);
+                    setActive({ value });
+                  }}
+                  key={index}
+                >
+                  {value}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </div>
+      <div className="header__choose-info">sdv</div>
+    </div>
+  );
 };
 
 export default Header;
